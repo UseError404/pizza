b82ec0b7f1cd7c81a0620af9774622f054bac87b
Osip <52411274+Osip-d25@users.noreply.github.com>
2024-10-15 16:03:54 +0300

    add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter basename="/pizza">
-            <Routes>
-                <Route  path="/" element={<App />} />
-            </Routes>
+            <App/>
         </BrowserRouter>
     </StrictMode>,
 );
